cbcbfc3d1e7e02823e823fc01b9320a776a828a9
Souhardya Chakraborty <64438285+souhardya1@users.noreply.github.com>
2022-08-26 16:30:41 +0530

    Add files via upload

---
+++ template-driven-forms/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsDemoComponent } from './forms-demo/forms-demo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormsDemoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ template-driven-forms/src/app/details.ts
@@ -0,0 +1,10 @@
+export class details{
+    constructor(public name:string,
+                public age:number,
+                public email:string,
+                public phone:number,
+                public password:string,
+                public gender:string,
+                public country:string
+                ) {}
+}
