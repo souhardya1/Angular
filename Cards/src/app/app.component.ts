45d451e4c8c48085d92a4364db7f584833cb6d4d
souhardya1 <kittu.chakrovartee@gmail.com>
2023-02-17 10:10:58 +0530

    Initial Commit

---
+++ Cards/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl:'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl:'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of snowy mountain'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl:'assets/biking.jpeg',
+      username: 'bikerz',
+      content: 'Love to ride this bike'
+    },
+  ];
+}
