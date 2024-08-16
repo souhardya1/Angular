1391859df58eeb7625f568d531d0a8fa18bfb78e
souhardya1 <kittu.chakrovartee@gmail.com>
2024-08-16 17:04:53 +0530

    Basic and nested form group

---
+++ Reactive-Forms/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SampleReactiveFormComponent } from './sample-reactive-form/sample-reactive-form.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SampleReactiveFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Reactive-Forms/src/app/sample-reactive-form/sample-reactive-form.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { log } from 'console';
+
+@Component({
+  selector: 'app-sample-reactive-form',
+  templateUrl: './sample-reactive-form.component.html',
+  styleUrl: './sample-reactive-form.component.css'
+})
+export class SampleReactiveFormComponent {
+
+  constructor() {}
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  prices: any[] = [
+    { value: 100, viewValue: '$ 100'},
+    { value: 200, viewValue: '$ 200'},
+    { value: 300, viewValue: '$ 300'}
+  ];
+
+  currency: any[] = [
+    { value: 'USD', viewValue: '$'},
+    { value: 'INR', viewValue: '₹'},
+    { value: 'EURO', viewValue: '€'}
+  ];
+
+  addBookForm: FormGroup;
+
+  private initForm(): void {
+    this.addBookForm = new FormGroup({
+      title: new FormControl(),
+      author: new FormControl(),
+      pages: new FormControl(),
+      price: new FormGroup({
+        currency: new FormControl(),
+        value: new FormControl()
+      }),
+      publishedDate: new FormControl(),
+      isPublished: new FormControl(),
+    });
+  }
+
+  save() {
+    if(this.addBookForm.valid) {
+      console.log('FormGroup');
+      console.log(this.addBookForm);
+      console.log('FormGroup Values');
+      console.log(this.addBookForm.value);
+    }
+
+  }
+
+}
